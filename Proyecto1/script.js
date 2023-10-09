d7c54df64d5c8de13217814ec53a6bc996887eaa
jorgelosada1 <126262020+jorgelosada1@users.noreply.github.com>
2023-10-09 11:54:31 -0500

    Add files via upload

---
+++ Proyecto1/script.js
@@ -0,0 +1,50 @@
+const pantalla  = document.querySelector(".Pantalla")
+const botones  = document.querySelectorAll(".boton")
+botones.forEach(boton => {
+    boton.addEventListener("click", ()=> {
+        const botonOn = boton.textContent;
+
+
+        if(boton.id === "borrar"){
+            if(pantalla.textContent.length ===1  || pantalla.te === "ERROR!!!"){
+                pantalla.textContent = "0"
+            }else{
+                pantalla.textContent = pantalla.textContent.slice(0, -1);
+            }
+            
+            return;
+        }
+
+
+        if(boton.id ==="igual"){
+            try{
+                pantalla.textContent = eval(pantalla.textContent);
+                if(pantalla.textContent === ""){
+                    pantalla.textContent = "ERROR!!!"
+                }
+            } catch{
+                pantalla.textContent = "ERROR!!!"
+            }
+
+            return;
+        }
+
+    
+
+        if(boton.id === "limpiar"){
+            pantalla.textContent = "0";
+            return;
+        }
+
+        if(pantalla.textContent === "0" || pantalla.textContent === "ERROR!!!"){
+            pantalla.textContent = botonOn;
+        }else{
+            pantalla.textContent += botonOn;
+        }
+
+        
+    } )
+    
+});
+
+
